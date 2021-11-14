b9b0a2240d039361adf2eba7888684f4d2dc8476
CoC12 <kouhei121613@gmail.com>
2021-11-14 17:42:43 +0900

    ログイン必須化

--- src/config/urls.py
+++ src/config/urls.py
@@ -21,6 +21,6 @@ import django_cas_ng.views
 urlpatterns = [
     path('', include('analytics.urls')),
     path('admin/', admin.site.urls),
-    path('accounts/login', django_cas_ng.views.LoginView.as_view(), name='cas_ng_login'),
-    path('accounts/logout', django_cas_ng.views.LogoutView.as_view(), name='cas_ng_logout'),
+    path('accounts/login/', django_cas_ng.views.LoginView.as_view(), name='cas_ng_login'),
+    path('accounts/logout/', django_cas_ng.views.LogoutView.as_view(), name='cas_ng_logout'),
 ]
