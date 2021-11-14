bc7d40f10f674b63b73a66d93479b6656c7bff5c
CoC12 <kouhei121613@gmail.com>
2021-11-14 17:53:30 +0900

    ダッシュボード実装

--- src/analytics/views/__init__.py
+++ src/analytics/views/__init__.py
@@ -1,1 +1,2 @@
 from .dashboard import dashboard_view  # noqa
+from .metrics_data import metrics_data_view  # noqa
--- src/analytics/views/dashboard.py
+++ src/analytics/views/dashboard.py
@@ -1,7 +1,16 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+from ..models import Metrics
 
 
-class DashboardView(TemplateView):
+class DashboardView(ListView):
     template_name = 'analytics/dashboard.html'
+    model = Metrics
+
+    def get_queryset(self, **kwargs):
+        return super().get_queryset(**kwargs).filter(
+            user=self.request.user,
+        ).prefetch_related(
+            'streamdata_set',
+        )
 
 dashboard_view = DashboardView.as_view()
